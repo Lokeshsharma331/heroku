f36196138b3b6edc5363daa98c29c7e352c3ffb0
Lokeshsharma331 <31363385+Lokeshsharma331@users.noreply.github.com>
2019-03-22 04:50:00 +0530

    Update web.py

--- web.py
+++ web.py
@@ -1,6 +1,6 @@
 from flask import Flask
 import pandas as pd
-from flask import request
+from flask import request,jsonify
 from sklearn import model_selection
 from sklearn.naive_bayes import GaussianNB
 app = Flask(__name__)
@@ -19,5 +19,6 @@ def home():
   cv_results = model_selection.cross_val_score(GaussianNB(), X_train, Y_train, cv=kfold, scoring=scoring)
 
   msg = "%s: %f (%f)" % ('NB accuracy', cv_results.mean(), cv_results.std())
-  return request.args.get("search")
+  data={"name":request.args.get("search")}
+  return jsonify(data)
   return msg
