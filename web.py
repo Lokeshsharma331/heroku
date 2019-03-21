fe36919336e6f31aa30b458c67ef02493dcc2176
Lokeshsharma331 <31363385+Lokeshsharma331@users.noreply.github.com>
2019-03-22 01:14:01 +0530

    Update web.py

--- web.py
+++ web.py
@@ -1,8 +1,24 @@
 from flask import Flask
 import pandas as pd
-app = Flask(__name__)
 
+from sklearn import model_selection
+from sklearn.naive_bayes import GaussianNB
+app = Flask(__name__)
 @app.route("/")
 def home():
   df=pd.read_csv('data.csv')
+  array=df.values
+  X = array[:,0:6]
+  Y = array[:,6]
+  X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=0.33)
+  clf = GaussianNB()
+  clf.fit(X_train, Y_train) 
+  pred_clf = clf.predict(X_validation)
+  kfold = model_selection.KFold(n_splits=10, random_state=5)
+  scoring="accuracy"
+  cv_results = model_selection.cross_val_score(GaussianNB(), X_train, Y_train, cv=kfold, scoring=scoring)
+
+  msg = "%s: %f (%f)" % ('NB accuracy', cv_results.mean(), cv_results.std())
+  print(msg)
+ 
   return "ssfb"
